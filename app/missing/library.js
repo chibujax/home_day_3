c693ce3b46e3761cbe56d9d43755a5b38ab760fa
chibujax <chibujax@gmail.com>
2017-02-16 08:04:46 +0100

    Refactor missing number function based on feedback

--- app/missing/library.js
+++ app/missing/library.js
@@ -1,17 +1,33 @@
 'strict'
 var file = {};
-file.findMissing =function(arr1, arr2) {
-     if (arr1.length !== arr2.length){
-        for (var i = 0; i < arr1.length; i++) {
-            for (var i = 0; i < arr2.length; i++) {
-                if (arr1[i] != arr2[i]) {
-                    return arr2[i];
-                }
-            }
+file.findMissing =function(aArray,bArray) {
+  if (aArray.length === bArray.length) {
+    return 0;
+  }
+  else {       
+    aArray.sort();
+    bArray.sort();  
+    var len = (aArray.length > bArray.length) ? bArray.length : aArray.length;
+    for (var i = 0; i <= len; i++) {
+      if(aArray[i] !== bArray[i]) {
+        if(aArray[i] === bArray[i + 1]) {
+          return bArray[i];
         }
+        else {
+          return aArray[i];
+        }
+      }
+      else {
+        if (aArray.length === i || bArray.length === i) {
+          if (bArray[i + 1] !== undefined) {
+            return bArray[i + 1];
+          }
+          else {
+            return aArray[i + 1];
+          }
+        }               
+      }
     }
-    if (arr1.length == arr2.length) {
-    	return 0;
-    }
+  }
 }
 module.exports = file;
